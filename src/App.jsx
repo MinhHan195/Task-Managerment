f585c6e7488a2c63cc829647b1e09c5f45c6da71
MinhHan195 <hanminhnguyen1905@gmail.com>
2026-03-23 20:44:49 +0700

    Khởi tạo dự án

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <div className="app">
+      <header>
+        <h1>My App</h1>
+      </header>
+      <main>
+        <p>Welcome to my app!</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
